57571f5c3d2a43b358c96e28305ab0d16f17f7dc
ZitRos <zitros.lab@gmail.com>
2017-10-19 21:12:54 +0300

    compile, deleteDocs and putDoc methods add

--- api.js
+++ api.js
@@ -6,16 +6,22 @@ const http = require('http')
 module.exports = ( conn ) => {
 
     const { host, port, username, password, path, version, ns } = conn
-    const headers = { 'Authorization': 'Basic ' + new Buffer( username + ':' + password ).toString( 'base64' ) }
+    const headers = {
+        'Authorization': 'Basic ' + new Buffer( username + ':' + password ).toString( 'base64' )
+    }
     
     const ok = ( res ) => res.statusCode == '200' 
     
     // factory 
     const OnResp = ( cb ) => ( res ) => {
-        if ( !ok(res) ) return cb({ code: res.statusCode })
         var data = ''
         res.on( 'data', chunk => { data += chunk } ) //collect data
-        res.on( 'end', () => cb( null, data ) )
+        res.on( 'end', () => {
+            if ( !ok(res) )
+                return cb({ code: res.statusCode, message: res.statusMessage }, data)
+            // todo: in next major version, decode data and check for any status.errors here
+            cb( null, data )
+        } )
     }
 
     
@@ -41,7 +47,7 @@ module.exports = ( conn ) => {
     const getDocNames = ( opts, cb ) => {
         
         opts = opts || {}
-        let generated = opts.generated || 0
+        let generated = +opts.generated || 0
         const url = `${path}${version}/${ns}/docnames?generated=${generated}`
 
         http.request( { headers, host, port, 'path': url }, OnResp( cb ) )
@@ -58,12 +64,98 @@ module.exports = ( conn ) => {
                 .end()
 
     }
+
+    const putDoc = ( name, doc, params, cb ) => {
+
+        if (typeof name !== "string")
+            throw new Error(`Document name must be a string (${ name })`);
+        if (!doc || typeof doc['enc'] === 'undefined' || !doc['content'])
+            throw new Error(`Invalid document data ${ JSON.stringify(doc) }`);
+        if (typeof params === "function" || !params) {
+            cb = params;
+            params = {};
+        }
+
+        const url = `${path}${version}/${ns}/doc/${name}${ 
+            typeof params.ignoreConflict !== 'undefined' 
+                ? '?ignoreConflict=' + +params.ignoreConflict
+                : ''
+        }`
+        const body = JSON.stringify(doc)
+        const req = http.request( {
+            method: 'PUT',
+            headers: Object.assign(
+                headers,
+                {
+                    'Content-Type': 'application/json',
+                    'Content-Length': body.length
+                },
+                params['If-None-Match'] ? { 'If-None-Match': params['If-None-Match'] } : undefined
+            ),
+            host, port,
+            path: url
+        } , cb ? OnResp( cb ) : undefined )
+        if (cb)
+            req.on( 'error', cb )
+        req.write( body )
+        req.end()
+
+    }
+
+    const compile = ( docNames, cb ) => {
+
+        if (!(docNames instanceof Array))
+            throw new Error(`Document names must be an array (${ docNames })`);
+
+        const url = `${path}${version}/${ns}/action/compile`
+        const body = JSON.stringify(docNames)
+        const req = http.request( {
+            method: 'POST',
+            headers: Object.assign(headers, {
+                'Content-Type': 'application/json',
+                'Content-Length': body.length
+            }),
+            host, port,
+            path: url
+        } , cb ? OnResp( cb ) : undefined )
+        if (cb)
+            req.on( 'error', cb )
+        req.write( body )
+        req.end()
+
+    }
+
+    const deleteDocs = ( docNames, cb ) => {
+
+        if (!(docNames instanceof Array))
+            throw new Error(`Document names must be an array (${ docNames })`);
+
+        const url = `${path}${version}/${ns}/docs`
+        const body = JSON.stringify(docNames)
+        const req = http.request( {
+            method: 'DELETE',
+            headers: Object.assign(headers, {
+                'Content-Type': 'application/json',
+                'Content-Length': body.length
+            }),
+            host, port,
+            path: url
+        } , cb ? OnResp( cb ) : undefined )
+        if (cb)
+            req.on( 'error', cb )
+        req.write( JSON.stringify(docNames) )
+        req.end()
+
+    }
         
     return {
-        headServer, 
+        compile,
+        deleteDocs,
         getServer,
         getDocNames,
-        getDoc
+        getDoc,
+        headServer,
+        putDoc
     }
 
 }
