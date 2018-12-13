f98785040ec5403276d0f8a0d8294bde3bb41b74
Iain Fletcher <iain.fletcher@rxmxcorp.com>
2018-12-13 11:15:18 +1000

    Add compile flags
    Signed-off-by: Iain Fletcher <iain.fletcher@rxmxcorp.com>

--- conn.js
+++ conn.js
@@ -3,6 +3,7 @@ module.exports = {
     host: '127.0.0.1',  port: 57772, 
     path: '/api/atelier/', version: 'v1', 
     ns: 'USER', 
-    username: '_SYSTEM', password: 'SYS'
+    username: '_SYSTEM', password: 'SYS',
+    compileflags: 'cuk'
 
 }
